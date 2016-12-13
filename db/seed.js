68f4efbd32366ba93e652df21d9ec38214a5a51a
Ally Cody <allysoncody2015@u.northwestern.edu>
2016-12-13 14:37:23 -0500

    changed location in seed

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,8 @@
 const db = require('APP/db')
 
 const seedCharities = () => db.Promise.map([
-  {name: 'Oxfam', email: 'Oxfam@dtf.com', image:'/empty',location:'HERE'},
-  {name: 'Unicef', email: 'Unicef@dtf.com', image:'/empty',location:'HERE'},
+  {name: 'Oxfam', email: 'Oxfam@dtf.com', image:'/empty',location: {type: 'Point', coordinates: [40.705691, -74.009342]}},
+  {name: 'Unicef', email: 'Unicef@dtf.com', image:'/empty',location:'{type: 'Point', coordinates: [40.705691, -74.009342]}},
 ], charities => db.model('charities').create(charities))
 
 db.didSync
