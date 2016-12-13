9fb43e4433c06d6e82717c22ec2c5dc423ab0ad2
Philhawk <pkjacob@gmail.com>
2016-12-13 13:54:56 -0500

    swipe is working

--- app/main.jsx
+++ app/main.jsx
@@ -3,32 +3,54 @@ import React from 'react'
 import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
 import {render} from 'react-dom'
 import {connect, Provider} from 'react-redux'
-
+import Tinderable from './components/IndexComponent'
 import store from './store'
-import Jokes from './components/Jokes'
 import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
 
-const ExampleApp = connect(
+const Home = connect(
   ({ auth }) => ({ user: auth })
 ) (
   ({ user, children }) =>
     <div>
       <nav>
         {user ? <WhoAmI/> : <Login/>}
-      </nav> 
+      </nav>
       {children}
     </div>
 )
 
+
+let data = [
+    {
+        title: 'A wonderful day',
+        text: '—— - ——— - - - ——— ———— - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— -',
+        image: 'portrait-1.jpg',
+        id: '1'
+    },
+    {
+        title: 'My amazing journey',
+        text: ' - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— - ————— - - ——— - - - ——— ———— ',
+        image: 'portrait-2.jpg',
+        id: '2'
+    },
+    {
+        title: 'Three recipes without cocoa',
+        text: ' - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— - ————— - - ——— - - - ———',
+        image: 'portrait-3.jpg',
+        id: '3'
+    },
+    {
+        title: 'Generic clickbait title',
+        text: ' —— ———— - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— - ————— - - ———— - ——— ',
+        image: 'portrait-4.jpg',
+        id: '4'
+    }
+];
+
 render (
   <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={ExampleApp}>
-        <IndexRedirect to="/jokes" />
-        <Route path="/jokes" component={Jokes} />
-      </Route>
-    </Router>
+    <Tinderable initialCardsData={data} />
   </Provider>,
   document.getElementById('main')
 )
