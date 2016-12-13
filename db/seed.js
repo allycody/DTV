2b8f7137dc8f806ea694316f00ab9b1aad3b3ead
Hussein Farah <husseinfarah93@gmail.com>
2016-12-13 14:36:17 -0500

    hardcoded 1 event as seed

--- db/seed.js
+++ db/seed.js
@@ -1,12 +1,37 @@
 const db = require('APP/db')
+const Event = require('./models/event')
+const Charity = require('./models/charity')
+
 
 const seedCharities = () => db.Promise.map([
   {name: 'Oxfam', email: 'Oxfam@dtf.com', image:'/empty',location:'HERE'},
   {name: 'Unicef', email: 'Unicef@dtf.com', image:'/empty',location:'HERE'},
 ], charities => db.model('charities').create(charities))
 
+const seedEvents = () => {
+  let globalCharity;
+  Charity.findById(1)
+  .then(charity => {
+    globalCharity = charity
+    return Event.create({
+      name:'Oxfam Event 1',
+      description:'Oxfams annual fundraiser',
+      date:'12/12',
+      location:90210,
+      image:'/url',
+      numNeeded:100,
+      urgent:true,
+      category:['fundraiser,outdoors']
+    })
+  })
+  .then(event => {
+    globalCharity.addEvent(event)
+  })
+}
+
 db.didSync
   .then(() => db.sync({force: true}))
   .then(() => seedCharities())
+  .then(() => seedEvents())
   .catch(error => console.error(error))    
-  .finally(() => db.close())
+  // .finally(() => db.close())
